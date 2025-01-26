0be1beace400fa8449e7ad41e1a8333a48d989c2
Valyakgg <knizhov@gmail.com>
2025-01-26 14:55:57 +0600

    1st try.

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+        val settingsBackButton = findViewById<Button>(R.id.settings_back_button)
+
+        settingsBackButton.setOnClickListener {
+            val backToMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(backToMainActivity)
+
+        }
+    }
+}
