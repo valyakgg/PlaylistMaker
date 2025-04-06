f4c742a9ba98c6075b2685292935394fe0f6e1ed
valyakgg <knizhov@gmail.com>
2025-04-06 22:51:04 +0600

    Спринт №13. 1st try.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.10")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.2.1")
 }
